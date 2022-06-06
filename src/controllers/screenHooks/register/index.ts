f8472992132734fd3ff6060a8eb2b1a33761ee86
Anyi Lozano <anyisitha12@gmail.com>
2022-06-05 20:37:12 -0500

    Changes In Login Y Register

--- src/controllers/screenHooks/register/index.ts
+++ src/controllers/screenHooks/register/index.ts
@@ -1,7 +1,18 @@
 import {useForm} from "react-hook-form";
 import {useState} from "react";
+import Swal from "sweetalert2";
+import useApi from "api";
+import {useHistory} from "react-router";
 
 const useRegister = () => {
+    // History
+    const history = useHistory();
+
+    // Api
+    const {useActions} = useApi();
+    const {dispatch, useRegisterActions} = useActions();
+    const {actCreateUser} = useRegisterActions();
+
     // States
     const [checkboxSelected, setCheckboxSelected] = useState<string>("");
     const [especialityCheckbox, setEspecialityCheckbox] = useState<string>("");
@@ -10,7 +21,65 @@ const useRegister = () => {
     const [check3, setCheck3] = useState<boolean>(false);
 
     // Hook form
-    const {control, setValue, formState: {isValid, errors}, setError} = useForm({mode: "onChange"});
+    const {
+        control,
+        setValue,
+        formState: {isValid, errors},
+        handleSubmit,
+        reset
+    } = useForm({mode: "onChange"});
+
+    // Handlers
+    const handleRegister = (data: any) => {
+        if(!check2){
+            Swal.fire({
+                title: "Error: Politica de HABEAS data",
+                text: "Debes aceptar nuestra politica de HABEAS data.",
+                icon: "error"
+            });
+        }else if(!check3){
+            Swal.fire({
+                title: "Error: Revisa el documento de valor",
+                text: "Debes aceptar el documento de transferencia de valor.",
+                icon: "error"
+            });
+        }else{
+            // @ts-ignore
+            dispatch(actCreateUser({
+                user: {...data, fullname: `${data.name} ${data.last_name}`},
+                onSuccess: () => {
+                    Swal.fire({
+                        icon: "success",
+                        title: "Bienvenido a Seranest!",
+                        text: "Ahora puedes ingresar a nuestra plataforma con el correo electronico y contraseña que ingresaste"
+                    }).then((res) => {
+                        if(res.isConfirmed){
+                            reset({
+                                name: "",
+                                last_name: "",
+                                email: "",
+                                document: "",
+                                confirm_document: "",
+                                document_type: "",
+                                phone: "",
+                                country: "",
+                                city: ""
+                            })
+                            history.push("/");
+                        }
+                    });
+                },
+                onError: () => {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Ohhh no!",
+                        text: "Ocurrio un problema al momento de crear el usuario intentalo de nuevo!"
+                    })
+                }
+            }))
+        }
+        console.log(data);
+    }
 
     return {
         control,
@@ -21,13 +90,14 @@ const useRegister = () => {
         especialityCheckbox,
         isValid,
         errors,
-        setError,
         check1,
         setCheck1,
         check2,
         setCheck2,
         check3,
-        setCheck3
+        setCheck3,
+        handleRegister,
+        handleSubmit
     };
 }
 
