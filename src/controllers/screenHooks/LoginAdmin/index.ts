5420252f4b7f7274c0eafd9819c562706835dc17
Anyi Lozano <anyisitha12@gmail.com>
2022-06-14 13:28:59 -0500

    pushorigin

--- src/controllers/screenHooks/LoginAdmin/index.ts
+++ src/controllers/screenHooks/LoginAdmin/index.ts
@@ -3,6 +3,7 @@ import useHelpers from "helpers";
 import { ILoginAdmin, ILoginAdminAction } from "models/interfaces/LoginAdmin.interfaces";
 import {useCallback, useEffect} from "react";
 import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 import useModels from "../../../models";
 
 const useLoginAdmin = () => {
@@ -34,7 +35,13 @@ const useLoginAdmin = () => {
         const request: ILoginAdminAction = {
             user: data,
             onSuccess: () => history.push("/admin"),
-            onError: (error: any) => console.log(error)
+            onError: (error: any) => {
+                console.log(error.response);
+                Swal.fire({
+                    icon: "error",
+                    title: "Su contraseña es incorrecta",
+                })
+            }
         }
 
         dispatch<any>(actLoginAdmin(request));
@@ -45,7 +52,21 @@ const useLoginAdmin = () => {
         const request: ILoginAdminAction = {
             user: data,
             onSuccess: () => history.push("/dashboard"),
-            onError: (error: any) => console.log(error)
+            onError: (error: any) => {
+                if(error.response.status === 404) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Usuario no encontrado!",
+                        text: "Valida los datos y vuelve a intentarlo",
+                    })
+                }else{
+                    Swal.fire({
+                        icon: "error",
+                        title: "Su usuario o contraseña son incorrectas",
+                        text: "Valida tu contrasena y vuelve a intentarlo",
+                    })
+                }
+            }
         }
 
         dispatch<any>(actLoginAdmin(request));
